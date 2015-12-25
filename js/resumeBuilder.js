90b0edb08ca0ec2c223b4e492d1154f8c02cca5a
David Silver <dsilver@candidatemetrics.com>
2015-12-25 13:50:07 -0800

    progressing through course

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%", "David Silver")
+var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer");
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
